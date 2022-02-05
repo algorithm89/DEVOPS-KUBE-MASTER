fe3e716c031ff7f268318b1c858cf6baac4c9756
devops <devops@kolobok.bubliks.net>
2022-02-05 18:56:52 -0500

    aa

--- firewall.sh
+++ firewall.sh
@@ -21,6 +21,9 @@ firewall-cmd --direct --add-rule ipv4 filter FORWARD 0 -i $int1 -o $int2 -m stat
 firewall-cmd --permanent --add-port=53/tcp --zone=internal
 firewall-cmd --permanent --add-port=53/udp --zone=internal
 
+firewall-cmd --zone=internal --permanent --add-port={6443,2379,2380,10250,10251,10252}/tcp
+
+
 
 cat /proc/sys/net/ipv4/ip_forward
 sysctl -w net.ipv4.ip_forward=1
