ec77b9633035a6b686ddb4a29609414b9fe89b0b
devops <devops@studios.net>
2022-03-16 12:19:29 -0400

    cool

--- nexus/nexexp.sh
+++ nexus/nexexp.sh
@@ -2,4 +2,9 @@
 
 #!/bin/bash
 
-docker run -d -p 9184:9184 -e NEXUS_HOST=http://10.1.1.1:8081 -e NEXUS_USERNAME=admin -e NEXUS_ADMIN_PASSWORD=toor89  ocadotechnology/nexus-exporter
+
+
+read -p "please put in your IP: " IP
+
+
+docker run -d -p 9184:9184 -e NEXUS_HOST=http://$IP:8081 -e NEXUS_USERNAME=admin -e NEXUS_ADMIN_PASSWORD=toor89  ocadotechnology/nexus-exporter
